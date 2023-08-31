d596e32075a619a7a58e9dc0c9d8cadade7e909a
globalsmile <mohammedbadmus823@gmail.com>
2023-08-31 05:51:30 -0700

    add: 111-bst_insert.c, 112-array_to_bst.c, 113-bst_search.c, 114-bst_remove.c files

--- binary_trees.h
+++ binary_trees.h
@@ -54,6 +54,12 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
 int bst_lesser(const binary_tree_t *node, int val);
 int bst_greater(const binary_tree_t *node, int val);
-
+bst_t *bst_insert(bst_t **tree, int value);
+bst_t *bst_insert_help(bst_t *node, int value);
+bst_t *array_to_bst(int *array, size_t size);
+bst_t *bst_search(const bst_t *tree, int value);
+bst_t *bst_remove(bst_t *root, int value);
+bst_t *bst_search(const bst_t *tree, int value);
+bst_t *bst_find_root(bst_t *node);
 
 #endif /* _BINARY_TREES_H_ */
