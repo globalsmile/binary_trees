a539ddb353a05f0718bfd166dbb8b5802c5e0d2d
globalsmile <mohammedbadmus823@gmail.com>
2023-08-31 16:40:48 -0700

    update: binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -65,7 +65,6 @@ bst_t *bst_search(const bst_t *tree, int value);
 bst_t *bst_remove(bst_t *root, int value);
 bst_t *bst_search(const bst_t *tree, int value);
 bst_t *bst_find_root(bst_t *node);
-int binary_tree_is_bst(const binary_tree_t *tree);
 int custom_binary_tree_height(const binary_tree_t *tree);
 
 #endif /* _BINARY_TREES_H_ */
